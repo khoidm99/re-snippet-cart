c99f08e1b7898deb347aedfa7d8cacacced1a9cd
Dao Minh Khoi <khoidm.bhsoft@gmail.com>
2020-06-02 15:29:04 +0700

    ok

--- component/Main/ProductList/Product/Product.js
+++ component/Main/ProductList/Product/Product.js
@@ -1,23 +1,35 @@
 import styles from './product.module.css';
 import { Button } from 'reactstrap';
 import Link from 'next/link';
-
+import { Container, Row, Col } from 'reactstrap';
 export default function Product(props) {
 	const { product } = props;
 	return (
-		<div className={styles.product}>
-			<h2 className={styles.title}>{product.name}</h2>
-			<p className={styles.description}>{product.description}</p>
-			<Link href="/product/[id]" as={`/product/${product.id}`}>
-				<img src={product.image} alt="" className={styles.image} width="100%" height="288px" />
-			</Link>
-
-			<div className={styles.price_button}>
-				<div className={styles.price}>${product.price.toFixed(2)}</div>
-				<Button className={styles.button} type="default">
-					Add to cart
-				</Button>
-			</div>
+		<div>
+			<Container>
+				<Row>
+					<Col lg="6">
+						<img src={product.image} width="480px" height="289px" />
+					</Col>
+					<Col lg="6">
+						<h1>{product.name}</h1>
+						<p>{product.description}</p>
+						<Container>
+							<Row>
+								<Col lg="6">
+									<p className="text-uppercase font-weight-bold">{product.price}</p>
+								</Col>
+								<Col lg="6">
+									<Button color="primary">Add to cart</Button>
+									<Link href="/product/[id]" as={`/product/${product.id}`}>
+										<Button color="danger" >View</Button>
+									</Link>
+								</Col>
+							</Row>
+						</Container>
+					</Col>
+				</Row>
+			</Container>
 		</div>
 	);
 }
